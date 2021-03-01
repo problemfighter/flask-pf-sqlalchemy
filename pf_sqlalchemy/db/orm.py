767b8151d307865b23de46058fee1fe29e5e866e
hmtmcse <hmtm.cse@gmail.com>
2021-03-01 10:46:49 +0600

    new module bismillah

---
+++ pf_sqlalchemy/db/orm.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+from util.common_util import get_uuid
+
+database = SQLAlchemy()
+
+
+class Base(database.Model):
+    __abstract__ = True
+    id = database.Column(database.BigInteger, primary_key=True)
+    created = database.Column(database.DateTime, default=database.func.now())
+    updated = database.Column(database.DateTime, default=database.func.now(), onupdate=database.func.now())
+    uuid = database.Column(database.String(50), default=get_uuid())
+    is_deleted = database.Column(database.Boolean, default=False)
---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Flask-PF-SqlAlchemy',
+    version='1.0',
+    url='https://github.com/problemfighter/flask-pf-sqlalchemy',
+    license='Apache 2.0',
+    author='Touhid Mia',
+    author_email='hmtm.cse@gmail.com',
+    description='This is Flask Problem Fighter SQLAlchemy Extension',
+    long_description=__doc__,
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'Flask-SQLAlchemy'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache 2.0 License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)

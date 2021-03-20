41c1580801f5c92346852d73312d5054b386a92c
hmtmcse <hmtm.cse@gmail.com>
2021-03-20 07:50:18 +0600

    adjusting crud operation

--- pf_sqlalchemy/db/orm.py
+++ pf_sqlalchemy/db/orm.py
@@ -11,3 +11,8 @@ class Base(database.Model):
     updated = database.Column(database.DateTime, default=database.func.now(), onupdate=database.func.now())
     uuid = database.Column(database.String(50), default=get_uuid())
     is_deleted = database.Column(database.Boolean, default=False)
+
+
+# Dummy Model
+class BaseModel(Base):
+    pass

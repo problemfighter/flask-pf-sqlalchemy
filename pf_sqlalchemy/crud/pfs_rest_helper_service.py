c49afec2ac3945abf199c9a7293bc6d913e27324
hmtmcse <hmtm.cse@gmail.com>
2021-03-24 23:10:45 +0600

    upgrading rest helper

--- pf_sqlalchemy/crud/pfs_rest_helper_service.py
+++ pf_sqlalchemy/crud/pfs_rest_helper_service.py
@@ -3,6 +3,7 @@ from pf_sqlalchemy.crud.pfs_crud_service import PfsCrudService
 from pf_sqlalchemy.db.orm import Base
 from pfms.pfapi.base.pfms_base_schema import PfBaseSchema
 from pfms.pfapi.rr.pfms_request_respons import PfRequestResponse
+from sqlalchemy import and_
 
 pfs_crud = PfsCrudService()
 
@@ -13,9 +14,11 @@ class PfsRestHelperService(PfRequestResponse):
     def __init__(self, model=None):
         self.model = model
 
-    def rest_check_id(self, dto_schema: PfBaseSchema, message="Invalid Id"):
-        if not dto_schema.id:
+    def rest_get_requested_model_id(self, message="Invalid Id"):
+        model_id = self.request().get_requested_data_value("id")
+        if not model_id:
             raise PFFCommonException(message)
+        return model_id
 
     def rest_get_id(self, dto_schema: PfBaseSchema, message="Invalid Id"):
         self.check_id(dto_schema, message)
@@ -28,9 +31,9 @@ class PfsRestHelperService(PfRequestResponse):
             raise PFFCommonException(message)
         return entity
 
-    def rest_get_value_by_id(self, model_id: int, entity: Base = None):
+    def rest_get_value_by_id(self, model_id: int, is_deleted: bool = False, entity: Base = None):
         entity = self._get_model(entity)
-        return entity.query.filter_by(id=model_id).first()
+        return entity.query.filter(and_(entity.id == model_id, entity.is_deleted == is_deleted)).first()
 
     def rest_create(self, request_dto: PfBaseSchema, response_dto: PfBaseSchema = None, message: str = "Successfully Created"):
         validated_model = self.json_request_process(request_dto)
@@ -39,16 +42,38 @@ class PfsRestHelperService(PfRequestResponse):
             return self.success(message)
         return self.json_data_response(validated_model, response_dto)
 
+    def rest_update(self, request_dto: PfBaseSchema, response_dto: PfBaseSchema = None, message: str = "Successfully Updated"):
+        model_id = self.rest_get_requested_model_id()
+        model = self.rest_get_value_by_id(model_id)
+        validated_model = self.json_request_process(request_dto, model)
+        pfs_crud.save(validated_model)
+        if not response_dto:
+            return self.success(message)
+        return self.json_data_response(validated_model, response_dto)
+
+    def rest_by_id_or_exception(self, model_id: int, message: str = "Requested data not exist!", is_deleted: bool = False):
+        model = self.rest_get_value_by_id(model_id, is_deleted)
+        if not model:
+            raise PFFCommonException(message)
+        return model
+
     def rest_hard_delete(self, model_id: int):
         pass
 
-    def rest_delete(self, model_id: int):
-        pass
+    def rest_delete(self, model_id: int, success: str = "Successfully Deleted", error: str = "Requested data not exist!"):
+        model = self.rest_by_id_or_exception(model_id, error)
+        model.is_deleted = True
+        pfs_crud.save(model)
+        return self.success(success)
 
-    def rest_details(self, model_id: int, response_dto: PfBaseSchema, message: str = "Invalid details request."):
-        model = self.rest_get_value_by_id(model_id)
-        if not model:
-            raise PFFCommonException(message)
+    def rest_restore(self, model_id: int, success: str = "Successfully Restored", error: str = "Requested data not exist!"):
+        model = self.rest_by_id_or_exception(model_id, error, True)
+        model.is_deleted = False
+        pfs_crud.save(model)
+        return self.success(success)
+
+    def rest_details(self, model_id: int, response_dto: PfBaseSchema, message: str = "Requested data not exist!"):
+        model = self.rest_by_id_or_exception(model_id, message)
         return self.json_data_response(model, response_dto)
 
     def rest_list(self, dto_schema: PfBaseSchema, pagination: bool = True, sort: bool = True):
